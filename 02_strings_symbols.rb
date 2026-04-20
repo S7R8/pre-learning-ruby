68018aea3aa411c6570394f61879955d5e7a2d81
S7R8 <southsid3@outlook.jp>
2026-04-21 06:57:14 +0900

    add: string周りまで

---
+++ 02_strings_symbols.rb
@@ -0,0 +1,25 @@
+# 1.to_s()
+# 1.to_s
+# 10.to_s(16)
+# 10.tos 16
+
+def add(a, b)
+  a + b
+end
+
+puts add(1, 2)
+# puts add (1, 2)
+
+s = 'Hello, world!'
+n = 123 * 2
+
+puts s, n.to_s
+
+puts "こんにちは\n世界"
+puts 'こんにちは\n世界'
+
+name = 'Alice'
+puts "Hello, #{name}!"
+
+puts 'ruby' == 'ruby'
+puts 'a' < 'b'
